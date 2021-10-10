c1c008710d50059a19a1fd746d600b1016ee0932
allesson182 <allessonqueiroz@gmail.com>
2021-10-10 12:31:34 -0300

    teste

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={textStyle.Text}>Esse é meu primeiro APP 2</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    // backgroundColor:  background: rgb(2,0,36);
+    backgroundColor: 'radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 0%, rgba(204,0,255,1) 93%)' ,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
+  
+const textStyle = StyleSheet.create( {
+  Text:{
+  color: '#00e3b2'
+  }
+})
