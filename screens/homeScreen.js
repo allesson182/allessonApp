de99c43b0032ca6458cffd440e8150d9660250fc
allesson182 <allessonqueiroz@gmail.com>
2021-11-21 12:36:58 -0300

    opção de login com o firebase adicionada

--- screens/homeScreen.js
+++ screens/homeScreen.js
@@ -1,11 +1,13 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import {Box, Button, FormControl, Heading, HStack, Input, Link, NativeBaseProvider, VStack} from "native-base";
 import {Text} from "react-native";
 import * as React from "react";
 import {StyleSheet} from "react-native";
-import { Icon } from 'react-native-elements'
-import {maxWidth} from "styled-system";
 import {FontAwesomeIcon} from "@fortawesome/react-native-fontawesome";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import {faDragon} from "@fortawesome/free-solid-svg-icons";
+import {useState} from "react";
 
 const style = StyleSheet.create({
     container: {
@@ -15,12 +17,47 @@ const style = StyleSheet.create({
         justifyContent: 'center'
     }
 });
+export default function HomeScreen({route,navigation}){
 
-export default class HomeScreen extends React.Component {
+        const[email, setEmail] = useState("");
+        const[senha, setSenha] = useState("");
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+            apiKey: "AIzaSyCMcoNWvUycx1FRDhJhYywtoD_nMEeFfIY",
+            authDomain: "allessonapp.firebaseapp.com",
+            projectId: "allessonapp",
+            storageBucket: "allessonapp.appspot.com",
+            messagingSenderId: "578322582923",
+            appId: "1:578322582923:web:0134dc8f4ae0fcc8e7338e",
+            measurementId: "G-KWDJ7PDSDQ"
+        };
+    // Initialize Firebase
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+function fazerLogin(){
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, senha)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log("Conectado")
+            navigation.navigate("Contatos")
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log("Erro:", errorMessage)
+        });
+
+}
 
 
-    render() {
+
         return (
+
             <NativeBaseProvider>
                 <Box style={style.container} safeArea flex={1} p="8" py="8" w="100%" mx="auto">
                     {/*<Icon    raised  name='headphones'  type='font-awesome'  color='#f50' size={100}/>*/}
@@ -42,7 +79,11 @@ export default class HomeScreen extends React.Component {
                                 }}>
                                 Login
                             </FormControl.Label>
-                            <Input  style={{color: 'white',width: 200}} />
+                            <Input  style={{color: 'white',width: 200}}
+                                    value={email}
+                                    onChangeText={email => setEmail(email)}
+                            />
+
                         </FormControl>
                         <FormControl>
                             <FormControl.Label
@@ -53,7 +94,9 @@ export default class HomeScreen extends React.Component {
                                 }}>
                                 Senha
                             </FormControl.Label>
-                            <Input type="password" style={{color: 'white', width: 200}} />
+                            <Input type="password" style={{color: 'white', width: 200}}
+                                   value={senha}
+                                   onChangeText={senha => setSenha(senha)}/>
                             <Link
                                 _text={{ fontSize: 'xs', fontWeight: '500', color: '#a5ded1' }}
                                 alignSelf="flex-end"
@@ -61,14 +104,14 @@ export default class HomeScreen extends React.Component {
                                 Esqueceu a senha?
                             </Link>
                         </FormControl>
-                        <Button onPress={() => this.props.navigation.navigate('Contatos')} mt="2" colorScheme="indigo" _text={{ color: 'white' }}>
+                        <Button onPress={() => {fazerLogin()}} mt="2" colorScheme="indigo" _text={{ color: 'white' }}>
                             Entrar
                         </Button>
                         <HStack mt="6" justifyContent="center">
                             <Text fontSize="sm" color="muted.700" fontWeight={400}>
                                 Novo usuario?{' '}
                             </Text>
-                            <Link onPress={() => this.props.navigation.navigate('SignUp')}
+                            <Link onPress={() => navigation.navigate('SignUp')}
                                 _text={{
                                     color: '#a5ded1' ,
                                     fontWeight: 'medium',
@@ -83,9 +126,5 @@ export default class HomeScreen extends React.Component {
             </NativeBaseProvider>
         )
 
-    };
-
-
-
-}
+    }
 
